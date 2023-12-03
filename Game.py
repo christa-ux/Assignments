1451e189f2334a123cf53e6979b5e9f3f90f1347
Christa <82023874+christa-ux@users.noreply.github.com>
2023-12-02 21:33:27 -0500

    Delete Game.py

--- Game.py
+++
@@ -1,8 +0,0 @@
-class Game:
-    def __init__(self, game_number, word, status, bad_guesses, score, missed_letters):
-        self.game_number = game_number
-        self.word = word
-        self.status = status
-        self.bad_guesses = bad_guesses
-        self.score = score
-        self.missed_letters = missed_letters
